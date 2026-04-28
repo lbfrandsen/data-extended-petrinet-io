34a7f644fcb03e85ac57a6efb49c62bae5977e41
Lucas Frandsen <s231316@dtu.dk>
2026-04-28 15:03:22 +0200

    Changed issue confirmation text.

--- public/index.js
+++ public/index.js
@@ -44,7 +44,7 @@ document.getElementById('js-report-bug').addEventListener('click', async () => {
 
     await showAlert({
       title: 'Bug report submitted',
-      message: `Issue #${result.number} created.\n${result.url}`
+      message: `Issue #${result.number} created.\nThank you for your help!♥️`
     });
   } catch (error) {
     await showAlert({
